09a5847284d3fa5bd12cb95398f34370cba1e369
Adrian Kummerländer <KnairdA@t-online.de>
2012-07-15 13:39:48 +0200

    Initial commit - basic but working version

---
+++ README.markdown
@@ -0,0 +1,45 @@
+# Spam Question Filter
+
+- Version: 1.0
+- Author: Adrian Kummerländer 
+- Build Date: 15th July 2012
+- Requirements: Symphony 2.*
+
+## Description
+
+Simple filter to check if a question like "2 + 3 = ?" was correctly answered by the user. 
+This can be used as a replacement for other spam prevention methods like captchas.
+
+## Installation
+
+1. Place the `spamquestionfilter` folder in your Symphony `extensions` directory.
+2. Go to _System > Extensions_, select "Spam Question Filter", choose "Enable" from the with-selected menu, then click Apply.
+
+## Usage
+
+1. Go to _Blueprints > Components_ and click the name of the event whose input you want to filter.
+2. In the "Filter Rules" field, select "Spam Question Filter"
+3. Save your event
+4. Now you will have to add three new fields to your frontend form, see the expample below for details.
+
+## Example-Form
+
+<!-- You have to add the math extension - otherwise we will not be able to generate the needed random numbers  -->
+<xsl:stylesheet version="1.0"
+	xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+	xmlns:math="http://exslt.org/math"
+	extension-element-prefixes="math">
+
+<!-- Fill the two numbers with random values  -->
+<xsl:param name="num1" select="floor(math:random()*10) + 1"/>
+<xsl:param name="num2" select="floor(math:random()*10) + 1"/>
+
+<!-- Test-Form  -->
+<form method="post" action="" enctype="multipart/form-data">
+<!-- This is the anti-spam question  -->
+	<xsl:value-of select="$num1"/> plus <xsl:value-of select="$num2"/> equals: <input name="fields[number]" type="text"/>
+
+<!-- These fields should be hidden so the user won't be confused by them  -->
+	<input name="fields[check1]" type="text" value="{$num1}" />
+	<input name="fields[check2]" type="text" value="{$num2}" />
+</form>
---
+++ extension.driver.php
@@ -0,0 +1,71 @@
+<?php
+
+	Class Extension_SpamQuestionFilter extends Extension {
+
+		public function about() {
+			return array(
+				'name' => 'Spam Question Filter',
+				'version' => '1.0',
+				'release-date' => '2012-07-15',
+				'author' => array(
+					'name' => 'Adrian Kummerländer',
+					'website' => 'http://adriank.redirectme.net',
+					'email' => 'adrian.kummerlaender@piraten-konstanz.de'
+				),
+				'description' => 'Protect your events against spam using a simple math question'
+			);
+		}
+
+		public function getSubscribedDelegates() {
+			return array(
+				array(
+					'page' => '/blueprints/events/new/',
+					'delegate' => 'AppendEventFilter',
+					'callback' => 'appendEventFilter'
+				),
+				array(
+					'page' => '/blueprints/events/edit/',
+					'delegate' => 'AppendEventFilter',
+					'callback' => 'appendEventFilter'
+				),
+				array(
+					'page' => '/frontend/',
+					'delegate' => 'EventPreSaveFilter',
+					'callback' => 'eventPreSaveFilter'
+				),
+			);
+		}
+
+		public function appendEventFilter(array $context) {
+			$context['options'][] = array(
+				'spam-question',
+				is_array($context['selected']) ? in_array('spam-question', $context['selected']) : false,
+				'Spam Question Filter'
+			);
+		}
+
+		public function eventPreSaveFilter(array $context) {
+			if(!in_array('spam-question', $context['event']->eParamFILTERS)) return;
+
+			$correct_answer = false;
+
+			if ( isset($context['fields']['check1']) &&
+				 isset($context['fields']['check2']) &&
+				 isset($context['fields']['number']) )
+			{
+				$result = $context['fields']['check1'] + $context['fields']['check2'];
+
+				if ( $result == $context['fields']['number'] ) 
+				{
+					$correct_answer = true;
+				}
+			}
+
+			if(in_array('spam-question', $context['event']->eParamFILTERS) && $correct_answer == false) {
+				$context['messages'][] = array(
+					'spam', false, __("The answer to the spam question was found to be incorrect")
+				);
+			}
+		}
+
+	}
