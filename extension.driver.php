b0dad13c8413b8c0d02f6951ac18eed39479d30a
Adrian Kummerländer <KnairdA@t-online.de>
2012-10-07 14:35:19 +0200

    Updated documentation

--- extension.driver.php
+++ extension.driver.php
@@ -83,29 +83,19 @@
 
 			$context['documentation'][] = new XMLElement('h3', 'Spam Question Filter');
 			$context['documentation'][] = new XMLElement('p', 'Prevents spam by asking the user to answer a simple math question like "2 + 4 = ?".');
-			$context['documentation'][] = new XMLElement('p', 'To use this filter you will have to make some changes to your markup - see the example below for further information:');
+			$context['documentation'][] = new XMLElement('p', 'To use this filter you will have to add the custom "Spamquestion" event to your page and build your form according to the example below:');
 
-			$code = '<!-- You have to add the math extension - otherwise we will not be able to generate the needed random numbers  -->
-<xsl:stylesheet version="1.0"
-	xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
-	xmlns:math="http://exslt.org/math"
-	extension-element-prefixes="math">
+			$code = '<form method="post" action="" enctype="multipart/form-data">
+<!-- This is the anti-spam question  -->
+    <xsl:value-of select="events/spamquestion/part1"/> plus <xsl:value-of select="events/spamquestion/part2"/> equals: <input name="fields[number]" type="text"/>
 
-<!-- Fill the two numbers with random values  -->
-<xsl:param name="num1" select="floor(math:random()*10) + 1"/>
-<xsl:param name="num2" select="floor(math:random()*10) + 1"/>
-
-<!-- Test-Form  -->
-<form method="post" action="" enctype="multipart/form-data">
-	<!-- This is the anti-spam question  -->
-	<xsl:value-of select="$num1"/> plus <xsl:value-of select="$num2"/> equals: <input name="fields[number]" type="text"/>
-
-	<!-- These fields should be hidden so the user won\'t be confused by them  -->
-	<input name="fields[check1]" type="text" value="{$num1}" />
-	<input name="fields[check2]" type="text" value="{$num2}" />
+<!-- These fields should be hidden so the user won\'t be confused by them  -->
+    <input name="fields[check1]" type="text" value="{events/spamquestion/part1}" />
+    <input name="fields[check2]" type="text" value="{events/spamquestion/part2}" />
 </form>';
 
 			$context['documentation'][] = contentBlueprintsEvents::processDocumentationCode($code);
+			$context['documentation'][] = new XMLElement('p', 'For further information concerning the installation and usage of this extension check the <a href="https://github.com/KnairdA/spamquestionfilter">README</a>.');
 
 		}
 
